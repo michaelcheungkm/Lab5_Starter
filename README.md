9cfacf3841425424cf5d3886f27cac359ee6ee58
Michael Cheung Ka Ming <michaelcheungkm@gmail.com>
2024-05-07 03:05:27 -0700

    explore

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Lab 5 - Starter
 Michael Cheung
+
+Expose: Different horns
+https://michaelcheungkm.github.io/Lab5_Starter/expose.html
--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -2,6 +2,50 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
+// set voices
+function setVoices (arrVoices, parent) {
+  arrVoices.forEach(voice => {
+    const voiceName = voice.name;
+    const newOption = document.createElement('option');
+    newOption.value = voiceName;
+    newOption.text = voiceName;
+    parent.appendChild(newOption);
+  })
+}
+
+function switchImage (img, src) {
+  img.src = src;
+}
+
 function init() {
-  // TODO
+  let utterance = new SpeechSynthesisUtterance();
+  const textArea = document.getElementById('text-to-speak');
+  const img = document.querySelector('img');
+  textArea.addEventListener('input', function() {
+    utterance.text = textArea.value;
+  });
+  const parent = document.getElementById('voice-select');
+  const synth = window.speechSynthesis;
+  synth.onvoiceschanged = () => {
+    const voices = synth.getVoices();
+    setVoices(voices, parent);
+  }
+  parent.addEventListener('change', function() {
+    const selectedVoice = parent.value;
+    const voices = window.speechSynthesis.getVoices();
+    const selectedVoiceObj = voices.find(voice => voice.name === selectedVoice);
+    if (selectedVoiceObj) {
+      utterance.voice = selectedVoiceObj;
+    }
+  });
+  const button = document.querySelector('button');
+  button.addEventListener('click', function() {
+    window.speechSynthesis.speak(utterance);
+  });
+  utterance.onstart = function(event) {
+    switchImage(img, 'assets/images/smiling-open.png');
+  }
+  utterance.onend = function(event) {
+    switchImage(img, 'assets/images/smiling.png');
+  }
 }
